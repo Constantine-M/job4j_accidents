895c5fb0399dee70f7294d763a20f8aa526ebc52
Constantine-M <mez365@mail.ru>
2024-06-19 15:09:54 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,6 +1,7 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -16,8 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/index")
-    public String getIndex() {
+    @GetMapping({"/index", "/"})
+    public String getIndex(Model model) {
+        model.addAttribute("user", "Consta");
         return "/index";
     }
 }
